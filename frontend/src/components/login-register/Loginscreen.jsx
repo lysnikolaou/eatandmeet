4cd3495ffdede6738f948674e1251648cda48858
NITZAN NASHI <nitzan14@gmail.com>
2019-05-22 20:23:33 +0200

    Configuration/frontend/init (#12)
    - add React framework via create-react-app
    - add CHANGELOG.md

---
+++ frontend/src/components/login-register/Loginscreen.jsx
@@ -0,0 +1,17 @@
+/**
+ * this is the container for switching between register and login
+ */
+
+import React, {Component} from 'react';
+
+class Loginscreen extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Loginscreen;

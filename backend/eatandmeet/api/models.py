afeddcf7226ac7b6d898ef5dbaa60c5c85ae0015
unknown <nickchrysaf@gmail.com>
2019-05-24 21:19:44 +0200

    User/Event Models Connected

--- backend/eatandmeet/api/models.py
+++ backend/eatandmeet/api/models.py
@@ -1,6 +1,7 @@
 from multiselectfield import MultiSelectField
 from django.db import models
-from django.contrib.auth.models import User
+
+from authentication.models import User
 
 
 TOPIC_CHOICES = [

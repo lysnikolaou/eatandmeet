db9a2411d7477584aa8baade046e75ced00b858b
Lysandros Nikolaou <lisandrosnik@gmail.com>
2019-05-24 19:03:18 +0200

    Backend/User Model Implementation (#26)
    * Backend: Authentication Implementation plus PyTest Configuration
    
    * Test User Model and Views

---
+++ backend/eatandmeet/authentication/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework import routers
+
+from .views import UserList, UserDetail
+
+
+urlpatterns = [
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail')
+]

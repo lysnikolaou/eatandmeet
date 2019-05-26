9abce16de847db408704f630c5a444fa340eabb9
NITZAN NASHI <nitzan14@gmail.com>
2019-05-27 00:03:52 +0200

    Revert "Frontend/feature/login register page (#34)"
    This reverts commit 3b61054efc1ced1c7802a15a2f87950b559fd8fc.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
-import { store } from './helpers/store';
-
-
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
-// import $ from 'jquery';
-// import Popper from 'popper.js';
-
-import './index.scss'
-
-import {App} from './App';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-// setup fake backend
-import { configureFakeBackend } from './helpers/fake_backend';
-configureFakeBackend();
-
-
-ReactDOM.render(    <Provider store={store}>
-    <App />
-</Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

24dd842287ea42c050b77e3cb94dc40515d9f87d
Lysandros Nikolaou <lisandrosnik@gmail.com>
2019-06-12 17:07:23 +0000

    Include About and Avatar fields in the User Model

--- backend/eatandmeet/authentication/models.py
+++ backend/eatandmeet/authentication/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
@@ -7,4 +8,16 @@ class User(AbstractUser):
     model that inherits from django.contrib.auth.models.AbstractUser, in case
     it needs to be expanded in the future.
     """
-    pass
+    about = models.CharField(
+        max_length=500,
+        blank=True,
+        null=True,
+        help_text='The About fields, where the user can provide some info'
+                  ' about themselves.'
+    )
+    avatar = models.ImageField(
+        upload_to='images/avatars/',
+        blank=True,
+        null=True,
+        help_text='The User\'s avatar to be shown on their profile.'
+    )

7543d531cba9df641c1713eb4878f002cee329ed
Nitzan Nashi <nitzan.nashi@zattoo.com>
2019-06-23 18:41:57 +0200

    update

--- frontend/src/features/entrance/index.jsx
+++ frontend/src/features/entrance/index.jsx
@@ -11,7 +11,7 @@ import {ProfilePage} from '../../components/UserProfile';
 
 import * as paths from './paths';
 import EventPage from '../../components/EventPage';
-import LoadingComponent from "../../components/loadingComponent";
+import LoadingComponent from '../../components/loadingComponent';
 
 class Entrance extends Component {
     render () {
@@ -41,7 +41,6 @@ class Entrance extends Component {
                 <MainLayoutRoute
                     path={paths.LOADER}
                     component={LoadingComponent}
-                    text="this is a demo"
                 />
 
                 <Redirect to={paths.FEED} />

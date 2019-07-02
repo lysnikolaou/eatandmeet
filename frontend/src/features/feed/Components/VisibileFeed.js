0312fa7074045b0f898bf392c10490ddd395b72e
Nitzan Nashi <nitzan.nashi@zattoo.com>
2019-07-03 00:16:28 +0200

    quick fix

--- frontend/src/features/feed/Components/VisibileFeed.js
+++ frontend/src/features/feed/Components/VisibileFeed.js
@@ -2,13 +2,6 @@ import FeedList from './feedList';
 import {connect} from 'react-redux';
 
 const getVisibileFeed = (feeds, date) => {
-    if (date) {
-        return feeds.filter((d) => {
-            return (
-                d.date.getTime() === date.getTime()
-            );
-        });
-    }
     return feeds;
 };
 
